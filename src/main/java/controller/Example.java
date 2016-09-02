9583c3bc20451b9adb6b65d4dc7c0fe518edeb12
hf <fan@klook.com>
2016-09-02 15:23:28 +0800

    spring boot 初始尝试

---
+++ src/main/java/controller/Example.java
@@ -0,0 +1,25 @@
+package controller;
+
+/**
+ * Created by hf on 16/9/2.
+ */
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/hf-boot")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
